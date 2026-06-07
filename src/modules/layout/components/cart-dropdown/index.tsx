ae3a9286212df73410b74229650b55312c34d268
djonova35 <djonova35@gmail.com>
2026-06-07 19:34:17 +0100

    Update index.tsx

--- src/modules/layout/components/cart-dropdown/index.tsx
+++ src/modules/layout/components/cart-dropdown/index.tsx
@@ -81,11 +81,23 @@ const CartDropdown = ({
     >
       <Popover className="relative h-full">
         <PopoverButton className="h-full">
-          <LocalizedClientLink
-            className="hover:text-ui-fg-base"
-            href="/cart"
-            data-testid="nav-cart-link"
-          >{`Cart (${totalItems})`}</LocalizedClientLink>
+         <LocalizedClientLink href="/cart" className="dj-nav-cart-icon" title="Cart">
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+  >
+    <path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z" />
+    <line x1="3" y1="6" x2="21" y2="6" />
+    <path d="M16 10a4 4 0 0 1-8 0" />
+  </svg>
+  {totalItems > 0 && (
+    <span className="dj-nav-cart-badge">{totalItems > 99 ? "99+" : totalItems}</span>
+  )}
+</LocalizedClientLink>
         </PopoverButton>
         <Transition
           show={cartDropdownOpen}
